3f31f2ee18475880ff826c86b245fc7ca65033aa
Snow Lukin <everyoursden@gmail.com>
2022-10-17 19:25:44 +0300

    Added lab13, individual task 2

--- pythonLabs/main.py
+++ pythonLabs/main.py
@@ -1,6 +1,7 @@
 import math
 import copy
 from itertools import combinations
+# import tkinter as tk
 
 # Обувная фабрика собирается начать выпуск элитной модели ботинок.
 # Дырочки для шнуровки будут расположены в два ряда, расстояние между рядами равно a,
@@ -353,7 +354,7 @@ def lab12_task5():
     print(f'Максимальная длина слова: {max_length}')
     print(f'Слова: {max_length_words}')
 
-lab12_task5()
+# lab12_task5()
 
 
 # Создание пароля посредством генерирования случайных символов может обернуться сложностью в
@@ -447,8 +448,300 @@ def individual_task_1(file_number):
     print(f'Ответ: {max_sum}')
 
 
+def lab13_task1():
+    class Cat:
+        def __init__(self, name, color, weight):
+            self.name = name
+            self.color = color
+            self.weight = weight
+
+        def meow(self):
+            print(f"Кот по имени {self.name}, весом {self.weight} кг и цветом {self.color}, мяукнул")
+
+    kitten = Cat("Алеша", "белый", 50)
+    kitten.meow()
+
+# lab13_task1()
+
+def lab13_task2():
+    class Animal:
+        def __init__(self, name):
+            self.name = name
+            print(f"Родилось животное {name}")
+
+        def eat(self):
+            print("Намнём")
+
+        def set_name(self, name):
+            self.name = name
+
+        def get_name(self):
+            return self.name
+
+        def make_noise(self):
+            print(f"{self.name} говорит: Гррр")
+    animal = Animal("Данила")
+    print(animal.get_name())
+    animal.set_name("Максим")
+    animal.eat()
+    animal.make_noise()
+
+# lab13_task2()
+
+def lab13_task3():
+    class StringVar:
+        def __init__(self, text):
+            self.string = str(text)
+
+        def set(self, text):
+            self.string = text
+
+        def get(self):
+            return self.string
+    string = StringVar("один")
+    print(string.get())
+    string.set("два")
+    print(string.get())
+
+# lab13_task3()
+
+def lab13_task4():
+    class Point:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+        def __str__(self):
+            return f"({self.x}, {self.y})"
+
+        def __add__(self, other):
+            return Point(self.x + other.x, self.y + other.y)
+
+        def __sub__(self, other):
+            return Point(self.x - other.x, self.y - other.y)
+
+        def __mul__(self, other):
+            return Point(self.x * other.x, self.y * other.y)
+
+        def __truediv__(self, other):
+            return Point(self.x / other.x, self.y / other.y)
+
+        def __floordiv__(self, other):
+            return Point(self.x // other.x, self.y // other.y)
+
+        def __mod__(self, other):
+            return Point(self.x % other.x, self.y % other.y)
+
+        def __pow__(self, other):
+            return Point(self.x ** other.x, self.y ** other.y)
+
+        def __lt__(self, other):
+            return self.x < other.x and self.y < other.y
+
+        def __le__(self, other):
+            return self.x <= other.x and self.y <= other.y
+
+        def __eq__(self, other):
+            return self.x == other.x and self.y == other.y
+
+        def __ne__(self, other):
+            return self.x != other.x and self.y != other.y
+
+        def __gt__(self, other):
+            return self.x > other.x and self.y > other.y
+
+        def __ge__(self, other):
+            return self.x >= other.x and self.y >= other.y
+
+    point1 = Point(1, 2)
+    point2 = Point(3, 4)
+    print(point1 + point2)
+    print(point1 - point2)
+    print(point1 * point2)
+    print(point1 / point2)
+    print(point1 // point2)
+    print(point1 % point2)
+    print(point1 ** point2)
+    print(point1 < point2)
+    print(point1 <= point2)
+    print(point1 == point2)
+    print(point1 != point2)
+    print(point1 > point2)
+    print(point1 >= point2)
+
+# lab13_task4()
+
+def lab13_task5():
+    class Animal:
+        def __init__(self, name):
+            self.name = name
+            print(f"Родилось животное {name}")
+
+        def eat(self):
+            print("Намнём")
+
+        def set_name(self, name):
+            self.name = name
+
+        def get_name(self):
+            return self.name
+
+        def make_noise(self):
+            print(f"{self.name} говорит: Гррр")
+
+    class Cat(Animal):
+        def __init__(self, name, color, weight):
+            super().__init__(name)
+            self.color = color
+            self.weight = weight
+            print(f"Родился кот по имени: {name}, весом {weight} кг, цветом {color}")
+
+        def meow(self):
+            print(f"Кот по имени {self.name}, весом {self.weight} кг и цветом {self.color}, мяукнул")
+
+        def make_noise(self):
+            print(f"{self.name} говорит: Мяу")
+    kitten = Cat("Алеша", "белый", 50)
+    kitten.meow()
+    kitten.make_noise()
+
+# lab13_task5()
+
+def lab13_task6():
+    class Animal:
+        def __init__(self, name):
+            self.name = name
+            print(f"Родилось животное {name}")
+
+        def eat(self):
+            print("Намнём")
+
+        def set_name(self, name):
+            self.name = name
+
+        def get_name(self):
+            return self.name
+
+        def make_noise(self):
+            print(f"{self.name} говорит: Гррр")
+
+    class Dog(Animal):
+        def __init__(self, name, color, weight):
+            super().__init__(name)
+            self.color = color
+            self.weight = weight
+            print(f"Родился собака по имени: {name}, весом {weight} кг, цветом {color}")
+
+        def bark(self):
+            print(f"Cобака {self.name}, весом {self.weight} кг, цветом {self.color}, гавкнула")
+
+        def make_noise(self):
+            print(f"{self.name} говорит: Гав")
+
+    dog = Dog("Алеша", "белый", 50)
+    dog.bark()
+    dog.make_noise()
+
+# lab13_task6()
+
+def lab13_task7():
+    class Animal:
+        def __init__(self, name):
+            self.name = name
+            print(f"Родилось животное {name}")
+
+        def eat(self):
+            print("Намнём")
+
+        def set_name(self, name):
+            self.name = name
+
+        def get_name(self):
+            return self.name
+
+        def make_noise(self):
+            print(f"{self.name} говорит: Гррр")
+
+    class Cat(Animal):
+        def __init__(self, name, color, weight):
+            super().__init__(name)
+            self.color = color
+            self.weight = weight
+            print(f"Родился кот по имени: {name}, весом {weight} кг, цветом {color}")
+
+        def meow(self):
+            print(f"Кот по имени {self.name}, весом {self.weight} кг и цветом {self.color}, мяукнул")
+
+        def make_noise(self):
+            print(f"{self.name} говорит: Мяу")
+
+    class Dog(Animal):
+        def __init__(self, name, color, weight):
+            super().__init__(name)
+            self.color = color
+            self.weight = weight
+            print(f"Родился собака по имени: {name}, весом {weight} кг, цветом {color}")
+
+        def bark(self):
+            print(f"Cобака {self.name}, весом {self.weight} кг, цветом {self.color}, гавкнула")
+
+        def make_noise(self):
+            print(f"{self.name} говорит: Гав")
+
+    cat = Cat("жч", "белый", 7)
+    cat.set_name("Железный человек")
+    cat.meow()
+    cat.make_noise()
+    dog = Dog("хлк", "черный", 10)
+    dog.set_name("Халк")
+    dog.bark()
+    dog.make_noise()
+    dog2 = Dog("анн", "красный", 5)
+    dog2.set_name("Анна")
+    dog2.bark()
+    dog2.make_noise()
+    animal = Animal("Александр")
+    animal.set_name("Саня")
+    animal.make_noise()
 
 
+def individual_task_2():
+    def print_matrix(matrix):
+        print('\n'.join('\t'.join(map(str, row)) for row in matrix))
+        print()
+
+    class Matrix:
+        def __init__(self, matrix):
+            self.matrix = matrix
+
+        def print_matrix(self):
+            print('\n'.join('\t'.join(map(str, row)) for row in self.matrix))
+            print()
+
+        def del_column(self, i):
+            self.matrix = [row[: i] + row[i + 1:] for row in self.matrix]
+
+        def del_row(self, i):
+            self.matrix = self.matrix[:i] + self.matrix[i + 1:]
+
+        def sub_matrix(self, start_row, end_row, start_column, end_column):
+            return [[self.matrix[i][j] for j in range(start_column, end_column + 1)] for i in range(start_row, end_row + 1)]
+    entity = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print("Initial matrix")
+    entity.print_matrix()
+
+    print("Sub matrix")
+    sub_matrix = entity.sub_matrix(1, 2, 1, 2)
+    print_matrix(sub_matrix)
+
+    print("Delete column")
+    entity.del_column(0)
+    entity.print_matrix()
+
+    print("Delete row")
+    entity.del_row(1)
+    entity.print_matrix()
+    
 
 # lab1_task6()
 # lab2_task1()
@@ -466,3 +759,11 @@ def individual_task_1(file_number):
 # individual_task_1(0) # тестовый вариант (Пример задания). Ответ: 36
 # individual_task_1(1) # первый файл. Ответ: 115110
 # individual_task_1(2) # второй файл. Ответ: 410884352
+# lab13_task1()
+# lab13_task2()
+# lab13_task3()
+# lab13_task4()
+# lab13_task5()
+# lab13_task6()
+# lab13_task7()
+# individual_task_2()
