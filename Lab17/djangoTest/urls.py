7173b80be5f9d5e8519bf0bfe723f925592ccf2a
Snow Lukin <everyoursden@gmail.com>
2022-12-11 12:37:58 +0300

    Design more or less finished

--- Lab17/djangoTest/urls.py
+++ Lab17/djangoTest/urls.py
@@ -9,6 +9,24 @@ urlpatterns = [
     path('articles', views.articles, name='articles'),
     path('ingredients', views.ingredients, name='ingredients'),
     path('recipes', views.recipes, name='recipes'),
+
     path('delete_chef/<int:entity_id>', views.delete_chef, name='delete_chef'),
     path('add_chef/', views.add_chef, name='add_chef'),
+    path('edit_chef/', views.edit_chef, name='edit_chef'),
+
+    path('delete_post/<int:entity_id>', views.delete_post, name='delete_post'),
+    path('add_post/', views.add_post, name='add_post'),
+    path('edit_post/', views.edit_post, name='edit_post'),
+
+    path('delete_article/<int:entity_id>', views.delete_article, name='delete_article'),
+    path('add_article/', views.add_article, name='add_article'),
+    path('edit_article/', views.edit_article, name='edit_article'),
+
+    path('delete_ingredient/<int:entity_id>', views.delete_ingredient, name='delete_ingredient'),
+    path('add_ingredient/', views.add_ingredient, name='add_ingredient'),
+    path('edit_ingredient/', views.edit_ingredient, name='edit_ingredient'),
+
+    path('delete_recipe/<int:entity_id>', views.delete_recipe, name='delete_recipe'),
+    path('add_recipe/', views.add_recipe, name='add_recipe'),
+    path('edit_recipe/', views.edit_recipe, name='edit_recipe'),
 ]
--- Lab17/djangoTest/views.py
+++ Lab17/djangoTest/views.py
@@ -4,11 +4,8 @@ from .models import Chef, Post, Article, Ingredient, Recipe
 from django.urls import reverse
 from django.shortcuts import redirect
 
+
 def index(request):
-    chefsObj = Chef.objects
-    context = {
-        'chefsObj': chefsObj,
-    }
     return render(request, 'djangoTest/index.html', {})
 
 
@@ -22,11 +19,13 @@ def chefs(request):
 
 def add_chef(request):
     objs = Chef.objects.order_by('-name')
-    # chef_id = len(objs)
+    chef_id = len(objs)
     name = request.POST['first']
     surname = request.POST['last']
     entity = Chef(name=name, surname=surname)
+    entity.id = chef_id     # to be able to edit entity in future
     entity.save()
+    print(entity.id)
     return redirect(reverse('djangoTest:chefs'))
 
 
@@ -34,36 +33,155 @@ def delete_chef(request, entity_id):
     entity = Chef.objects.get(id=entity_id)
     entity.delete()
     return redirect(reverse('djangoTest:chefs'))
-    # return HttpResponseRedirect(reverse('djangoTest/chefs'))
+
+
+def edit_chef(request):
+    entity_id = request.POST['edit_id']
+    # Would be cool to check before updating but nah
+    entity = Chef.objects.get(id=entity_id)
+    entity.name = request.POST['edit_first']
+    entity.surname = request.POST['edit_last']
+    entity.save()
+    return redirect(reverse('djangoTest:chefs'))
 
 
 def posts(request):
-    objs = list(Post.objects)
+    objs = Post.objects.order_by('-title')
     context = {
         'objs': objs,
     }
     return render(request, 'djangoTest/posts.html', context)
 
 
+def add_post(request):
+    objs = Post.objects.order_by('-title')
+    entity_id = len(objs)
+    title = request.POST['first']
+    body = request.POST['last']
+    entity = Post(title=title, body=body)
+    entity.id = entity_id     # to be able to edit entity in future
+    entity.save()
+    return redirect(reverse('djangoTest:posts'))
+
+
+def delete_post(request, entity_id):
+    entity = Post.objects.get(id=entity_id)
+    entity.delete()
+    return redirect(reverse('djangoTest:posts'))
+
+
+def edit_post(request):
+    entity_id = request.POST['edit_id']
+    # Would be cool to check before updating but nah
+    entity = Post.objects.get(id=entity_id)
+    entity.title = request.POST['edit_first']
+    entity.body = request.POST['edit_last']
+    entity.save()
+    return redirect(reverse('djangoTest:posts'))
+
+
 def articles(request):
-    objs = list(Article.objects)
+    objs = Article.objects.order_by('-title')
     context = {
         'objs': objs,
     }
     return render(request, 'djangoTest/articles.html', context)
 
 
+def add_article(request):
+    objs = Article.objects.order_by('-title')
+    entity_id = len(objs)
+    title = request.POST['first']
+    body = request.POST['last']
+    entity = Article(title=title, body=body)
+    entity.id = entity_id     # to be able to edit entity in future
+    entity.save()
+    return redirect(reverse('djangoTest:articles'))
+
+
+def delete_article(request, entity_id):
+    entity = Article.objects.get(id=entity_id)
+    entity.delete()
+    return redirect(reverse('djangoTest:articles'))
+
+
+def edit_article(request):
+    entity_id = request.POST['edit_id']
+    # Would be cool to check before updating but nah
+    entity = Article.objects.get(id=entity_id)
+    entity.title = request.POST['edit_first']
+    entity.body = request.POST['edit_last']
+    entity.save()
+    return redirect(reverse('djangoTest:articles'))
+
+
 def ingredients(request):
-    objs = list(Ingredient.objects)
+    objs = Ingredient.objects.order_by('-name')
     context = {
         'objs': objs,
     }
     return render(request, 'djangoTest/ingredients.html', context)
 
 
+def add_ingredient(request):
+    objs = Ingredient.objects.order_by('-name')
+    entity_id = len(objs)
+    name = request.POST['first']
+    cost = int(request.POST['last'])
+    entity = Ingredient(name=name, cost=cost)
+    entity.id = entity_id     # to be able to edit entity in future
+    entity.save()
+    return redirect(reverse('djangoTest:ingredients'))
+
+
+def delete_ingredient(request, entity_id):
+    entity = Ingredient.objects.get(id=entity_id)
+    entity.delete()
+    return redirect(reverse('djangoTest:ingredients'))
+
+
+def edit_ingredient(request):
+    entity_id = request.POST['edit_id']
+    # Would be cool to check before updating but nah
+    entity = Post.objects.get(id=entity_id)
+    entity.name = request.POST['edit_first']
+    entity.cost = int(request.POST['edit_last'])
+    entity.save()
+    return redirect(reverse('djangoTest:ingredients'))
+
+
 def recipes(request):
-    objs = list(Recipe.objects)
+    objs = Recipe.objects.order_by('-title')
     context = {
         'objs': objs,
     }
     return render(request, 'djangoTest/recipes.html', context)
+
+
+def add_recipe(request):
+    objs = Recipe.objects.order_by('-title')
+    entity_id = len(objs)
+    title = request.POST['first']
+    body = request.POST['second']
+    cost = request.POST['third']
+    entity = Recipe(title=title, body=body, cost=cost)
+    entity.id = entity_id     # to be able to edit entity in future
+    entity.save()
+    return redirect(reverse('djangoTest:recipes'))
+
+
+def delete_recipe(request, entity_id):
+    entity = Recipe.objects.get(id=entity_id)
+    entity.delete()
+    return redirect(reverse('djangoTest:recipes'))
+
+
+def edit_recipe(request):
+    entity_id = request.POST['edit_id']
+    # Would be cool to check before updating but nah
+    entity = Recipe.objects.get(id=entity_id)
+    entity.title = request.POST['edit_first']
+    entity.body = request.POST['edit_second']
+    entity.price = int(request.POST['edit_third'])
+    entity.save()
+    return redirect(reverse('djangoTest:recipes'))
